4826fe3c8c48653bd374295e04db171ee716fe55
Andrew Anisimov <anisimov.new@gmail.com>
2021-10-04 14:16:13 +0300

    Binaries update script.
    TODO: implement dotnet update

--- ton-sdk.sh
+++ ton-sdk.sh
@@ -11,8 +11,9 @@ PULL_NODE_SE=1
 NODE_SE_IMAGE=tonlabs/local-node:latest
 NODE_SE_PORT=8888
 BINARIES_ARCHIVE_PREFIX=tonclient_dotnet_bridge
-BINARIES_SOURCE_PATH=$(realpath ../ton-sdk-dotnet-bridge)
+BINARIES_SOURCE_PATH=$(realpath ../ton-client-dotnet-bridge)
 BINARIES_DOWNLOAD_PATH=$(realpath ../binaries)
+BINARIES_SDK_BRANCH=
 DOTNET_SOURCE_PATH=$(realpath ../ton-client-dotnet)
 PHP_EXT_SOURCE_PATH=$(realpath ../ton-client-php-ext)
 PHP_SOURCE_PATH=$(realpath ../ton-client-php)
@@ -20,7 +21,10 @@ SDK_SOURCE_PATH=$(realpath ../TON-SDK)
 SKIP_PHP_EXT=0
 SKIP_TESTS=0
 SDK_VERSION_TAG=
+SKIP_BINARIES_BUILD=0
+SKIP_BINARIES_DOWNLOAD=0
 SKIP_BINARIES_COPY=0
+IGNORE_BUILD_FAILURES=0
 VERBOSE=0
 WAIT=1
 HELP=$(
@@ -31,12 +35,16 @@ ${SCRIPT} <command> [<options>]
 Common options:
 
   -v    - Verbose output.
+  -i    - Ignore workflow build failures globally.
 
 List of commands with command-specific options:
 
   binaries  - Build SDK binaries.
     Options:
+      -b          - Specify SDK branch to use. By default, TON SDK is built using the tag name
+                    matching the given one via the -t argument.
       -d <PATH>   - Path for binaries download directory (default is ${BINARIES_DOWNLOAD_PATH}).
+      -D          - Skip download.
       -p <PATH>   - Path to binaries project sources (default is ${BINARIES_SOURCE_PATH}).
       -s <PATH>   - SDK source path (${SDK_SOURCE_PATH} by default).
       -t <TAG>    - SDK version tag.
@@ -91,6 +99,10 @@ EXAMPLES
 
   ./${SCRIPT} update -t X.Y.Z
 
+  Build new binaries for SDK version X.Y.Z using source code from branch master:
+
+  ./${SCRIPT} binaries -t X.Y.Z -b master
+
 EOF
 )
 
@@ -102,17 +114,30 @@ fi
 COMMAND=$1
 shift 1
 
-while getopts "Bd:hlLp:s:t:TvwWx:X" opt; do
+help() {
+  echo "${HELP}"
+  exit 0
+}
+
+while getopts "b:Bd:DhilLp:s:t:TvwWx:X" opt; do
 case ${opt} in
+  b)
+    BINARIES_SDK_BRANCH=$OPTARG
+    ;;
   B)
     SKIP_BINARIES_COPY=1
     ;;
   d)
     BINARIES_DOWNLOAD_PATH=$OPTARG
     ;;
+  D)
+    SKIP_BINARIES_DOWNLOAD=1
+    ;;
   h)
-    echo "${HELP}"
-    exit 0
+    help
+    ;;
+  i)
+    IGNORE_BUILD_FAILURES=1
     ;;
   l)
     PULL_NODE_SE=1
@@ -164,6 +189,26 @@ verbose() {
   fi
 }
 
+wait_for_build_if_needed() {
+  BUILD_NAME=$1
+  if [ "${WAIT}" -eq 1 ]; then
+    verbose "Waiting for build ${BUILD_NAME} to finish"
+    RUN_ID=$(gh run list --limit 1 | grep "${BUILD_NAME}" | perl -pe "s/.*${BUILD_NAME}.*?([0-9]+?)\s.*/\$1/g")
+    while [ "${RUN_ID}" = "" ]; do
+      verbose "Workflow not running. Waiting 10 seconds"
+      sleep 10
+      RUN_ID=$(gh run list --limit 1 | grep "${BUILD_NAME}" | perl -pe "s/.*${BUILD_NAME}.*?([0-9]+?)\s.*/\$1/g")
+    done
+    if [ "${IGNORE_BUILD_FAILURES}" -eq "0" ]; then
+      gh run watch --exit-status "${RUN_ID}"
+    else
+      gh run watch "${RUN_ID}"
+    fi
+  else
+    verbose "Don't wait for build ${BUILD_NAME}"
+  fi
+}
+
 run() {
   if [ "${PULL_NODE_SE}" -eq "1" ]; then
     verbose "Pulling image ${PULL_NODE_SE}"
@@ -189,24 +234,92 @@ check_binaries_downloaded() {
   fi
 }
 
+download_binaries() {
+
+  if [ "${SKIP_BINARIES_DOWNLOAD}" -eq "1" ]; then
+    verbose "Skipping binaries download."
+    return
+  fi
+
+  BUILD_NAME=$1
+  RUN_ID=$(gh run list --limit 1 | grep "${BUILD_NAME}" | perl -pe "s/.*${BUILD_NAME}.*?([0-9]+?)\s.*/\$1/g")
+  if [ "${RUN_ID}" = "" ]; then
+    verbose "Workflow not found. Exiting"
+  fi
+
+  ARTIFACTS_DOWNLOAD_PATH="${BINARIES_DOWNLOAD_PATH}/${SDK_VERSION_TAG}"
+  if mkdir -p "${ARTIFACTS_DOWNLOAD_PATH}"; then
+    verbose "Downloading artifacts for run ${RUN_ID} into ${ARTIFACTS_DOWNLOAD_PATH}"
+    gh run download "${RUN_ID}" -D "${ARTIFACTS_DOWNLOAD_PATH}"
+  else
+    echo "Unable to create directory ${ARTIFACTS_DOWNLOAD_PATH}"
+    exit 1
+  fi
+
+}
+
+build_new_binaries() {
+  verbose "Building new binaries"
+
+  CD=$(pwd)
+  cd "${BINARIES_SOURCE_PATH}"
+
+  BINARIES_TAG=$SDK_VERSION_TAG
+  if [ ! "${BINARIES_SDK_BRANCH}" = "" ]; then
+    BINARIES_TAG="${BINARIES_TAG}-${BINARIES_SDK_BRANCH}"
+  fi
+
+  EXISTING_TAG=$(git tag -l "${BINARIES_TAG}")
+  if [ "${EXISTING_TAG}" = "${BINARIES_TAG}" ]; then
+    verbose "Binaries for SDK tag ${BINARIES_TAG} are already built"
+  else
+    verbose "Create new tag ${BINARIES_TAG}"
+    if git tag "${BINARIES_TAG}"; then
+      if ! git push origin "${BINARIES_TAG}"; then
+        echo "Failed to push tag ${BINARIES_TAG}"
+        exit 1
+      fi
+    else
+      verbose "Tag ${BINARIES_TAG} already exists."
+    fi
+    wait_for_build_if_needed "${BINARIES_TAG}"
+  fi
+
+  download_binaries "${BINARIES_TAG}"
+
+  cd "${CD}"
+}
+
 update_binaries() {
+
   verbose "Updating TON SDK binaries to ${SDK_VERSION_TAG}"
-  mkdir -p "${BINARIES_DOWNLOAD_PATH}"
-  # TODO: implement!
+  if ! mkdir -p "${BINARIES_DOWNLOAD_PATH}"; then
+    echo "Unable to create directory ${BINARIES_DOWNLOAD_PATH}"
+    exit 1
+  fi
+
+  if [ "${SKIP_BINARIES_BUILD}" -eq "0" ]; then
+    build_new_binaries
+  fi
 }
 
 extract_binary() {
   BINARY_SUFFIX=$1
   ZIP_PATH=$2
   EXTRACT_PATH=$3
-  ZIP_ARCHIVE="${BINARIES_DOWNLOAD_PATH}/${SDK_VERSION_TAG}/${BINARIES_ARCHIVE_PREFIX}_${BINARY_SUFFIX}_${SDK_VERSION_TAG}.zip"
-  verbose "Extracting ${ZIP_ARCHIVE}:${ZIP_PATH} to ${EXTRACT_PATH}"
-  unzip -p "${ZIP_ARCHIVE}" "${ZIP_PATH}" > "${EXTRACT_PATH}"
-}
-
-update_dotnet() {
-  verbose "Updating TON .NET SDK to ${SDK_VERSION_TAG}"
-  # TODO: implement!
+  ZIP_DIR_PATH="${BINARIES_DOWNLOAD_PATH}/${SDK_VERSION_TAG}/${BINARIES_ARCHIVE_PREFIX}_${BINARY_SUFFIX}_${SDK_VERSION_TAG}"
+  if [ -d "${ZIP_DIR_PATH}" ]; then
+    verbose "Archive is already extracted into ${ZIP_DIR_PATH}"
+    verbose "Copying ${ZIP_DIR_PATH}/${ZIP_PATH} to ${EXTRACT_PATH}"
+    cp "${ZIP_DIR_PATH}/${ZIP_PATH}" "${EXTRACT_PATH}"
+  else
+    ZIP_ARCHIVE="${ZIP_DIR_PATH}.zip"
+    verbose "Extracting ${ZIP_ARCHIVE}:${ZIP_PATH} to ${EXTRACT_PATH}"
+    if ! unzip -p "${ZIP_ARCHIVE}" "${ZIP_PATH}" > "${EXTRACT_PATH}"; then
+      echo "Failed to extract ${ZIP_ARCHIVE}:${ZIP_PATH} into ${EXTRACT_PATH}"
+      exit 1
+    fi
+  fi
 }
 
 replace_in_files() {
@@ -216,33 +329,82 @@ replace_in_files() {
     verbose "Nothing to replace"
   else
     shift 2
-      for file in "$@"
-      do
-        verbose "Replacing ${SEARCH} with ${REPLACE} in ${file}"
-        sed -i '' "s|${SEARCH}|${REPLACE}|g" "${file}"
-      done
+    for file in "$@"
+    do
+      verbose "Replacing ${SEARCH} with ${REPLACE} in ${file}"
+      sed -i '' "s|${SEARCH}|${REPLACE}|g" "${file}"
+    done
   fi
+}
+
+update_dotnet() {
+  verbose "Updating TON .NET SDK to ${SDK_VERSION_TAG}"
+
+  CD=$(pwd)
+
+  verbose "Checking out SDK at ${SDK_VERSION_TAG}"
+  cd "${SDK_SOURCE_PATH}" && git checkout "${SDK_VERSION_TAG}"
+  cd "${DOTNET_SOURCE_PATH}"
+
+  if [ "${SKIP_BINARIES_COPY}" -eq 0 ]; then
+    check_binaries_downloaded
+    extract_binary linux_x64 lib/libton_client.so runtimes/linux-x64/native/libton_client.so
+    extract_binary macos_x64 lib/libton_client.dylib runtimes/osx-x64/native/libton_client.dylib
+    extract_binary windows_x64 bin/ton_client.dll runtimes/win-x64/native/ton_client.dll
+    extract_binary windows_x86 bin/ton_client.dll runtimes/win-x86/native/ton_client.dll
+  else
+    verbose "Skip binaries copy"
+  fi
+
+  CURRENT_SDK_VERSION=$(head -n 1 generator/api.index.txt)
+  verbose "Found current SDK version: ${CURRENT_SDK_VERSION}"
+  if [ ! "${CURRENT_SDK_VERSION}" = "${SDK_VERSION_TAG}" ]; then
+
+    verbose "Copying api.json"
+    cp "${SDK_SOURCE_PATH}/tools/api.json" generator/api.json
+
+    verbose "Generating new code from api.json"
+    cd generator && npm run generate && cd ..
+
+  else
+    verbose "SDK version ${SDK_VERSION_TAG} is already built"
+  fi
+
+  verbose "Copying test contracts"
+  cp -rf "${SDK_SOURCE_PATH}/ton_client/src/tests/contracts" tests/Resources
+
+  VERSION_FILES=$(grep -r "${CURRENT_SDK_VERSION}" -l . | grep -v '/bin/' | grep -v '/runtimes/' | grep -v '.git/' | tr '\n' ' ')
+  verbose "Replacing version in files ${VERSION_FILES}"
+  replace_in_files "${CURRENT_SDK_VERSION}" "${SDK_VERSION_TAG}" ${VERSION_FILES}
+
+  if [ "${SKIP_TESTS}" -eq "0" ]; then
+    verbose "Running local tests"
+    TON_NETWORK_ADDRESS=http://localhost:${NODE_SE_PORT} dotnet test
+  else
+    verbose "Skip running local tests"
+  fi
+
+  verbose "Pushing changes to master branch"
+  git add .
+  if git commit -a -m "Upgrade to SDK version ${SDK_VERSION_TAG}."; then
+    git push origin master
+    wait_for_build_if_needed master
+  else
+    verbose "Nothing to commit"
+  fi
+
+  verbose "Pushing new version tag ${SDK_VERSION_TAG}"
+  if git tag "${SDK_VERSION_TAG}"; then
+    git push origin "${SDK_VERSION_TAG}"
+    wait_for_build_if_needed "$SDK_VERSION_TAG"
+  fi
+
+  cd "${CD}"
 }
 
 check_php_version() {
   verbose "Checking PHP version"
   # TODO: implement!
-}
-
-wait_for_build_if_needed() {
-  BUILD_NAME=$1
-  if [ "${WAIT}" -eq 1 ]; then
-    verbose "Waiting for build ${BUILD_NAME} to finish"
-    RUN_ID=$(gh run list --limit 1 | grep "${BUILD_NAME}" | perl -pe "s/.*${BUILD_NAME}.*?([0-9]+?)\s.*/\$1/g")
-    while [ "${RUN_ID}" = "" ]; do
-      verbose "Workflow not running. Waiting 10 seconds"
-      sleep 10
-      RUN_ID=$(gh run list --limit 1 | grep "${BUILD_NAME}" | perl -pe "s/.*${BUILD_NAME}.*?([0-9]+?)\s.*/\$1/g")
-    done
-    gh run watch --exit-status "${RUN_ID}"
-  else
-    verbose "Don't wait for build ${BUILD_NAME}"
-  fi
 }
 
 update_php_ext() {
@@ -334,6 +496,7 @@ update_php() {
   fi
 
   verbose "Commit and push sources"
+  git add .
   if git commit -a -m "Upgrade to SDK version ${SDK_VERSION_TAG}."; then
     git push origin master
     wait_for_build_if_needed master
